57b1155d361ee3b1b6cb667009cef8fd5867cda6
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-10-09 20:18:22 -0700

    Use "media_type" over "content_type"

--- cdp_backend/utils/file_utils.py
+++ cdp_backend/utils/file_utils.py
@@ -9,19 +9,19 @@ import dask.dataframe as dd
 ###############################################################################
 
 
-def get_content_type(uri: str) -> Optional[str]:
+def get_media_type(uri: str) -> Optional[str]:
     # Media types retrieved from:
     # http://www.iana.org/assignments/media-types/media-types.xhtml
-    content_types = dd.read_csv(
+    media_types = dd.read_csv(
         str(Path(__file__).parent / "resources" / "content-types-*.csv")
     )
 
     # Get suffix from URI
     splits = uri.split(".")
-    tail = splits[-1]
+    suffix = splits[-1]
 
     # Find content type
-    matching = content_types[content_types["Name"] == tail].compute()
+    matching = media_types[media_types["Name"] == suffix].compute()
 
     # If there is exactly one matching type, return it
     if len(matching) == 1:
