9fbcd17cef1a1534ee967a75a55e267df8690f30
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-10-30 23:07:53 -0700

    Add example-infrastructure dir with files to be mirrored to cookiecutter

---
+++ example-infrastructure/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is just used for dev setup and CI stack preview.
+It is recommended to use the cookiecutter-cdp-deployment as it
+"""
+
+from pulumi import get_stack, export
+
+from cdp_backend.infrastructure import CDPStack
+
+###############################################################################
+
+cdp_stack = CDPStack(get_stack())
+
+export("gcp_project_name", cdp_stack.gcp_project.name)
+export("firestore_address", cdp_stack.firestore_app.app_id)
+export("gcp_bucket_name", cdp_stack.firestore_app.default_bucket)
