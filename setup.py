ae50409e68579c23f7f4e9e66f566f4d4fc9b437
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-12-12 12:27:44 -0800

    Bump version: 3.0.0.dev1 → 3.0.0.dev2

--- setup.py
+++ setup.py
@@ -102,6 +102,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-backend",
     # Do not edit this string manually, always use bump2version
     # Details in CONTRIBUTING.rst
-    version="3.0.0.dev1",
+    version="3.0.0.dev2",
     zip_safe=False,
 )
