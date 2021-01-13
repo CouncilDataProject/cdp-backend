3129850b0037a649eda6cecc471dad5fb8c9fc01
isaac219 <isaac219@gmail.com>
2021-01-13 00:50:14 -0800

    Created file store functions with gcsfs and fsppec

---
+++ cdp_backend/file_store/functions.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from pathlib import Path
+from typing import Optional
+
+from fsspec.implementations.local import LocalFileSystem
+from gcsfs import GCSFileSystem
+
+###############################################################################
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)4s: %(module)s:%(lineno)4s %(asctime)s] %(message)s",
+)
+log = logging.getLogger(__name__)
+
+GCS_URI = "gs://{bucket}/{filename}"
+
+###############################################################################
+
+
+def initialize_gcs_file_system(credentials_file: str) -> GCSFileSystem:
+    return GCSFileSystem(token=credentials_file)
+
+
+def get_file_uri(fs: GCSFileSystem, bucket: str, filename: str) -> str:
+    if fs.exists(f"{bucket}/{filename}"):
+        return GCS_URI.format(bucket=bucket, filename=filename)
+
+    return None
+
+
+def upload_file(
+    fs: GCSFileSystem,
+    bucket: str,
+    filepath: str,
+    save_name: Optional[str] = None,
+    remove_local: bool = False,
+) -> str:
+
+    # Resolve the path to enforce path complete
+    filepath = Path(filepath).resolve(strict=True)
+
+    # Create save name if none provided
+    if not save_name:
+        save_name = filepath.name
+
+    # Try to get the file first
+    uri = get_file_uri(fs, bucket, save_name)
+
+    # Return existing uri and remove local copy if desired
+    if uri:
+        if remove_local:
+            remove_local_file(filepath)
+
+        return uri
+
+    # If no existing file, upload and remove local copy if desired
+    else:
+        save_url = GCS_URI.format(bucket=bucket, filename=save_name)
+
+        fs.put_file(filepath, f"{bucket}/{save_name}")
+
+        if remove_local:
+            remove_local_file(filepath)
+
+        return save_url
+
+
+def remove_local_file(filepath: str) -> None:
+    fs = LocalFileSystem()
+    fs.rm(filepath)
+
+    log.info(f"Removed {filepath} from local file system.")
---
+++ cdp_backend/tests/file_store/test_functions.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+from tempfile import NamedTemporaryFile
+from typing import Optional, Union
+from unittest import mock
+
+import pytest
+from gcsfs import GCSFileSystem
+
+from cdp_backend.file_store import functions
+
+###############################################################################
+
+FILENAME = "file.txt"
+BUCKET = "bucket"
+FILEPATH = "fake/path/" + FILENAME
+SAVE_NAME = "fakeSaveName"
+EXISTING_FILE_URI = "gs://bucket/existing_file.json"
+GCS_FILE_URI = functions.GCS_URI.format(bucket=BUCKET, filename=FILENAME)
+
+###############################################################################
+
+
+def test_initialize_gcs_file_system() -> None:
+    with mock.patch("gcsfs.GCSFileSystem.connect"):
+        assert isinstance(
+            functions.initialize_gcs_file_system("path/to/credentials"), GCSFileSystem
+        )
+
+
+@pytest.mark.parametrize(
+    "filename, bucket, exists, expected",
+    [
+        (
+            FILENAME,
+            BUCKET,
+            True,
+            functions.GCS_URI.format(bucket=BUCKET, filename=FILENAME),
+        ),
+        (FILENAME, BUCKET, False, None),
+    ],
+)
+def test_get_file_uri(
+    filename: str,
+    bucket: str,
+    exists: bool,
+    expected: Union[str, None],
+) -> None:
+    with mock.patch("gcsfs.GCSFileSystem.connect"):
+        with mock.patch("gcsfs.GCSFileSystem.exists") as mock_exists:
+            mock_exists.return_value = exists
+
+            assert expected == functions.get_file_uri(
+                functions.initialize_gcs_file_system("fake/path"), bucket, filename
+            )
+
+    return
+
+
+@pytest.mark.parametrize(
+    "bucket, filepath, save_name, remove_local, existing_file_uri, expected",
+    [
+        (BUCKET, FILEPATH, SAVE_NAME, True, EXISTING_FILE_URI, EXISTING_FILE_URI),
+        (BUCKET, FILEPATH, SAVE_NAME, False, EXISTING_FILE_URI, EXISTING_FILE_URI),
+        (BUCKET, FILEPATH, None, False, None, GCS_FILE_URI),
+    ],
+)
+def test_upload_file(
+    bucket: str,
+    filepath: str,
+    save_name: Optional[str],
+    remove_local: bool,
+    existing_file_uri: str,
+    expected: str,
+) -> None:
+    with mock.patch("gcsfs.GCSFileSystem") as mock_fs:
+        with mock.patch(
+            "cdp_backend.file_store.functions.get_file_uri"
+        ) as mock_file_uri:
+            with mock.patch(
+                "cdp_backend.file_store.functions.remove_local_file"
+            ):
+                with mock.patch("pathlib.Path.resolve") as mock_path:
+                    mock_file_uri.return_value = existing_file_uri
+                    mock_path.return_value.name = FILENAME 
+
+                    assert expected == functions.upload_file(
+                        mock_fs, bucket, filepath, save_name, remove_local
+                    )
+
+    return
+
+
+def test_remove_local_file() -> None:
+    f = NamedTemporaryFile()
+    f.write(b"Hello world!")
+
+    assert os.path.isfile(f.name)
+
+    functions.remove_local_file(f.name)
+
+    assert not os.path.isfile(f.name)
