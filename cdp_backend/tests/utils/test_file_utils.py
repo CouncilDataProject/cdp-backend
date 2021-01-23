aff39ea64157091423a2b41bb7fb7223db78ac60
isaac219 <isaac219@gmail.com>
2021-01-23 11:18:44 -0800

    Bringing external resource copy from cdptools

--- cdp_backend/tests/utils/test_file_utils.py
+++ cdp_backend/tests/utils/test_file_utils.py
@@ -2,12 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from typing import Optional
+from unittest import mock
 
 import pytest
 
 from cdp_backend.utils import file_utils
+from cdp_backend.utils.file_utils import external_resource_copy
 
 #############################################################################
+
+
+@pytest.fixture
+def example_video(data_dir):
+    return data_dir / "example_video.mp4"
+
+
+class MockedResponse:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.opened = open(self.filepath, "rb")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, tb):
+        self.opened.close()
+
+    def raise_for_status(self):
+        return True
+
+    @property
+    def raw(self):
+        return self.opened
+
+
+@pytest.fixture
+def mocked_request(example_video):
+    with mock.patch("requests.get") as MockRequest:
+        MockRequest.return_value = MockedResponse(example_video)
+        yield MockRequest
 
 
 @pytest.mark.parametrize(
@@ -30,3 +63,8 @@ from cdp_backend.utils import file_utils
 def test_get_media_type(uri: str, expected_result: Optional[str]) -> None:
     actual_result = file_utils.get_media_type(uri)
     assert actual_result == expected_result
+
+
+def test_external_resource_copy(tmpdir, mocked_request):
+    save_path = tmpdir / "tmpcopy.mp4"
+    external_resource_copy("https://doesntmatter.com/example.mp4", save_path)
--- cdp_backend/utils/file_utils.py
+++ cdp_backend/utils/file_utils.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import logging
+import shutil
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
 import dask.dataframe as dd
+import requests
+
+###############################################################################
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)4s: %(module)s:%(lineno)4s %(asctime)s] %(message)s",
+)
+log = logging.getLogger(__name__)
 
 ###############################################################################
 
@@ -43,3 +53,45 @@ def get_media_type(uri: str) -> Optional[str]:
 
     # Otherwise, return none
     return None
+
+
+def external_resource_copy(
+    uri: str, dst: Optional[Union[str, Path]] = None, overwrite: bool = False
+) -> Path:
+    """
+    Copy an external resource to a local destination on the machine.
+    Parameters
+    ----------
+    uri: str
+        The uri for the external resource to copy.
+    dst: Optional[Union[str, Path]]
+        A specific destination to where the copy should be placed. If None provided
+        stores the resource in the current working directory.
+    overwrite: bool
+        Boolean value indicating whether or not to overwrite a local resource with
+        the same name if it already exists.
+    Returns
+    -------
+    saved_path: Path
+        The path of where the resource ended up getting copied to.
+    """
+    if dst is None:
+        dst = uri.split("/")[-1]
+
+    # Ensure dst doesn't exist
+    dst = Path(dst).resolve()
+    if dst.is_dir():
+        dst = dst / uri.split("/")[-1]
+    if dst.is_file() and not overwrite:
+        raise FileExistsError(dst)
+
+    # Open requests connection to uri as a stream
+    log.debug(f"Beginning external resource copy from: {uri}")
+    with requests.get(uri, stream=True) as streamed_read:
+        streamed_read.raise_for_status()
+        with open(dst, "wb") as streamed_write:
+            shutil.copyfileobj(streamed_read.raw, streamed_write)
+    log.debug(f"Completed external resource copy from: {uri}")
+    log.info(f"Stored external resource copy: {dst}")
+
+    return dst
