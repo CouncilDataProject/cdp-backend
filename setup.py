ece69be3a18fb43ef4b6e9d9224740585e48a374
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-04-15 22:17:07 -0700

    Add bin scripts to setup

--- setup.py
+++ setup.py
@@ -85,6 +85,8 @@ setup(
     ),
     entry_points={
         "console_scripts": [
+            "clean_cdp_database=cdp_backend.bin.clean_cdp_database:main",
+            "clean_cdp_filestore=cdp_backend.bin.clean_cdp_filestore:main",
             "create_cdp_database_uml=cdp_backend.bin.create_cdp_database_uml:main",
             (
                 "create_cdp_ingestion_models_doc="
