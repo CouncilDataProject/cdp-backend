861956c52fb0788e19b9608654c7f072b205265c
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-07-25 13:04:47 -0700

    Add tests for the rest of the pipeline

--- cdp_backend/tests/pipeline/test_event_gather_pipeline.py
+++ cdp_backend/tests/pipeline/test_event_gather_pipeline.py
@@ -3,6 +3,7 @@
 
 import sys
 from pathlib import Path
+from typing import List
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -10,6 +11,12 @@ import pytest
 from prefect import Flow
 
 from cdp_backend.pipeline import event_gather_pipeline as pipeline
+from cdp_backend.pipeline.ingestion_models import (
+    EXAMPLE_FILLED_EVENT,
+    EXAMPLE_MINIMAL_EVENT,
+    EventIngestionModel,
+    Session,
+)
 from cdp_backend.pipeline.mock_get_events import (
     FILLED_FLOW_CONFIG,
     MANY_FLOW_CONFIG,
@@ -17,9 +24,22 @@ from cdp_backend.pipeline.mock_get_events import (
     RANDOM_FLOW_CONFIG,
 )
 from cdp_backend.pipeline.pipeline_config import EventGatherPipelineConfig
+from cdp_backend.pipeline.transcript_model import EXAMPLE_TRANSCRIPT, Transcript
 
 #############################################################################
 
+# NOTE:
+# unittest mock patches are accesible in reverse order in params
+# i.e. if we did the following patches
+# @patch(module.func_a)
+# @patch(module.func_b)
+#
+# the param order for the magic mocks would be
+# def test_module(func_b, func_a):
+#
+# great system stdlib :upsidedownface:
+
+PIPELINE_PATH = "cdp_backend.pipeline.event_gather_pipeline"
 VIDEO_CONTENT_HASH = "7490ea6cf56648d60a40dd334e46e5d7de0f31dde0c7ce4d85747896fdd2ab42"
 
 #############################################################################
@@ -38,8 +58,8 @@ def test_create_event_gather_flow(config: EventGatherPipelineConfig) -> None:
     sys.platform == "win32",
     reason="Path handling / splitting failing due to windows path separator",
 )
-@mock.patch("cdp_backend.file_store.functions.get_file_uri")
-@mock.patch("cdp_backend.file_store.functions.upload_file")
+@mock.patch(f"{PIPELINE_PATH}.fs_functions.get_file_uri")
+@mock.patch(f"{PIPELINE_PATH}.fs_functions.upload_file")
 @pytest.mark.parametrize(
     "get_file_uri_value, audio_upload_file_return, expected_session_content_hash",
     [
@@ -56,8 +76,8 @@ def test_create_event_gather_flow(config: EventGatherPipelineConfig) -> None:
     ],
 )
 def test_get_video_and_split_audio(
-    mock_get_file_uri: MagicMock,
     mock_upload_file: MagicMock,
+    mock_get_file_uri: MagicMock,
     get_file_uri_value: str,
     audio_upload_file_return: str,
     expected_session_content_hash: str,
@@ -78,3 +98,146 @@ def test_get_video_and_split_audio(
     # Check outputs
     assert session_content_hash == expected_session_content_hash
     assert audio_uri == audio_upload_file_return
+
+
+@pytest.mark.parametrize(
+    "event, expected_phrases",
+    [
+        (EXAMPLE_MINIMAL_EVENT, ["Full Council"]),
+        (
+            EXAMPLE_FILLED_EVENT,
+            # Note: the order here is because under the hood, we are using a set and
+            # casting to a list.
+            # It's the item addition order that matters, not the order of the phrases
+            # So this is completely fine
+            [
+                "AN ORDINANCE relating to the financing of the West Seattle Bridge",
+                "Teresa Mosqueda",
+                "Andrew Lewis",
+                "Inf 1656",
+                "Full Council",
+                "M. Lorena González",
+                "Chair",
+                "Alex Pedersen",
+                "Council President",
+                "CB 119858",
+                "Vice Chair",
+            ],
+        ),
+    ],
+)
+def test_construct_speech_to_text_phrases_context(
+    event: EventIngestionModel, expected_phrases: List[str]
+) -> None:
+    phrases = pipeline.construct_speech_to_text_phrases_context.run(  # type: ignore
+        event,
+    )
+
+    assert set(phrases) == set(expected_phrases)
+
+
+@mock.patch(f"{PIPELINE_PATH}.fs_functions.get_file_uri")
+@mock.patch(f"{PIPELINE_PATH}.use_speech_to_text_and_generate_transcript.run")
+@mock.patch(f"{PIPELINE_PATH}.fs_functions.upload_file")
+@pytest.mark.parametrize(
+    "mock_speech_to_text_return, "
+    "mock_upload_transcript_return, "
+    "session, "
+    "event",
+    [
+        # Testing no captions case
+        (
+            EXAMPLE_TRANSCRIPT,
+            "ex://abc123-transcript.json",
+            EXAMPLE_MINIMAL_EVENT.sessions[0],
+            EXAMPLE_MINIMAL_EVENT,
+        ),
+        # Testing captions case
+        (
+            None,
+            "ex://abc123-transcript.json",
+            EXAMPLE_FILLED_EVENT.sessions[1],
+            EXAMPLE_FILLED_EVENT,
+        ),
+    ],
+)
+def test_generate_transcript(
+    mock_upload_transcript: MagicMock,
+    mock_speech_to_text: MagicMock,
+    mock_get_transcript_uri: MagicMock,
+    mock_speech_to_text_return: Transcript,
+    mock_upload_transcript_return: str,
+    session: Session,
+    event: EventIngestionModel,
+) -> None:
+    mock_get_transcript_uri.return_value = None
+    mock_speech_to_text.return_value = mock_speech_to_text_return
+    mock_upload_transcript.return_value = mock_upload_transcript_return
+
+    with Flow("Test Generate Transcript") as flow:
+        pipeline.generate_transcript(
+            session_content_hash="abc123",
+            audio_uri="fake://doesn't-matter.wav",
+            session=session,
+            event=event,
+            bucket="bucket",
+            credentials_file="fake/creds.json",
+        )
+
+    # Run the flow
+    state = flow.run()
+
+    # Check state and results
+    assert state.is_successful()
+
+
+@mock.patch(f"{PIPELINE_PATH}.db_functions.upload_db_model")
+@pytest.mark.parametrize(
+    "event, session_processing_results",
+    [
+        (
+            EXAMPLE_MINIMAL_EVENT,
+            [
+                pipeline.SessionProcessingResult(
+                    session=EXAMPLE_MINIMAL_EVENT.sessions[0],
+                    audio_uri="ex://abc123-audio.wav",
+                    transcript=EXAMPLE_TRANSCRIPT,
+                    transcript_uri="ex://abc123-transcript.json",
+                    static_thumbnail_uri="ex://abc123-static-thumbnail.png",
+                    hover_thumbnail_uri="ex://abc123-hover-thumbnail.gif",
+                ),
+            ],
+        ),
+        (
+            EXAMPLE_FILLED_EVENT,
+            [
+                pipeline.SessionProcessingResult(
+                    session=EXAMPLE_FILLED_EVENT.sessions[0],
+                    audio_uri="ex://abc123-audio.wav",
+                    transcript=EXAMPLE_TRANSCRIPT,
+                    transcript_uri="ex://abc123-transcript.json",
+                    static_thumbnail_uri="ex://abc123-static-thumbnail.png",
+                    hover_thumbnail_uri="ex://abc123-hover-thumbnail.gif",
+                ),
+                pipeline.SessionProcessingResult(
+                    session=EXAMPLE_FILLED_EVENT.sessions[1],
+                    audio_uri="ex://def456-audio.wav",
+                    transcript=EXAMPLE_TRANSCRIPT,
+                    transcript_uri="ex://def456-transcript.json",
+                    static_thumbnail_uri="ex://def456-static-thumbnail.png",
+                    hover_thumbnail_uri="ex://def456-hover-thumbnail.gif",
+                ),
+            ],
+        ),
+    ],
+)
+def test_store_event_processing_results(
+    mock_upload_db_model: MagicMock,
+    event: EventIngestionModel,
+    session_processing_results: List[pipeline.SessionProcessingResult],
+) -> None:
+    pipeline.store_event_processing_results.run(
+        event=event,
+        session_processing_results=session_processing_results,
+        credentials_file="fake/credentials.json",
+    )
