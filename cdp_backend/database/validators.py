2d04bbb590755be92eeff351d4dc37c3ddd37882
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-07-26 00:37:56 -0700

    Change uniqueness log msg to be less scary

--- cdp_backend/database/validators.py
+++ cdp_backend/database/validators.py
@@ -77,7 +77,7 @@ def get_model_uniqueness(model: Model) -> UniquenessValidation:
     # Fetch and assert single value
     results = list(query.fetch())
     if len(results) >= 1:
-        log.warning(f"Found conflicting results={results} for model={model}.")
+        log.info(f"Found existing or conflicting results={results} for model={model}.")
         return UniquenessValidation(is_unique=False, conflicting_models=results)
 
     return UniquenessValidation(is_unique=True, conflicting_models=results)
