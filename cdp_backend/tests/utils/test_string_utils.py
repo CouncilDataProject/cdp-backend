814202b435be92ad8d445781b4cfe9b29a617d7e
isaac219 <isaac219@gmail.com>
2021-09-05 20:14:36 -0400

    Add unit tests

--- cdp_backend/tests/utils/test_string_utils.py
+++ cdp_backend/tests/utils/test_string_utils.py
@@ -27,3 +27,13 @@ from cdp_backend.utils import string_utils
 )
 def test_clean_text(text: str, expected: str, clean_stop_words: bool) -> None:
     assert string_utils.clean_text(text, clean_stop_words=clean_stop_words) == expected
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("https://storage.googleapis.com/download/storage/v1/b/bucket.appspot.com/o/wombo_combo.mp4?alt=media", "gs://bucket.appspot.com/wombo_combo.mp4"),
+        ("https://storage.googleapis.com/download/storage/bucket.appspot.com/o/wombo_combo.mp4?alt=media", ""),
+    ],
+)
+def test_convert_gcs_json_url_to_gsutil_form(text: str, expected: str) -> None:
+    assert string_utils.convert_gcs_json_url_to_gsutil_form(text) == expected
