9fad7da0f5b7e919130174eddf289b4929365966
evamaxfield <evamaxfieldbrown@gmail.com>
2022-08-19 00:44:09 +0000

    Deploying to gh-pages from @ CouncilDataProject/cdp-backend@1e5b80743c4c0374f5a7d7df9731346d61f02f55 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g5c25bee',
+    VERSION: '0.1.dev1+g1e5b807',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
